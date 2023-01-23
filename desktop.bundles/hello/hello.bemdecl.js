61c4232d7b10d8415ca6d4ca4e0eb89c2687fe38
Natalia <lazorenko.natasha@yandex.ru>
2023-01-23 23:13:46 +0800

    added files

---
+++ desktop.bundles/hello/hello.bemdecl.js
@@ -0,0 +1,101 @@
+exports.blocks = [
+    {
+        "name": "page"
+    },
+    {
+        "name": "page",
+        "mods": [
+            {
+                "name": "theme",
+                "vals": [
+                    {
+                        "name": "body"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "css"
+            }
+        ]
+    },
+    {
+        "name": "page",
+        "elems": [
+            {
+                "name": "js"
+            }
+        ]
+    },
+    {
+        "name": "body-inner"
+    },
+    {
+        "name": "section"
+    },
+    {
+        "name": "top-header"
+    },
+    {
+        "name": "top-header",
+        "elems": [
+            {
+                "name": "logo"
+            }
+        ]
+    },
+    {
+        "name": "top-header",
+        "elems": [
+            {
+                "name": "search-form"
+            }
+        ]
+    },
+    {
+        "name": "top-header",
+        "elems": [
+            {
+                "name": "phone"
+            }
+        ]
+    },
+    {
+        "name": "menu-section"
+    },
+    {
+        "name": "menu"
+    },
+    {
+        "name": "header"
+    },
+    {
+        "name": "content-section"
+    },
+    {
+        "name": "content"
+    },
+    {
+        "name": "content",
+        "elems": [
+            {
+                "name": "news"
+            }
+        ]
+    },
+    {
+        "name": "content",
+        "elems": [
+            {
+                "name": "content-info"
+            }
+        ]
+    },
+    {
+        "name": "footer"
+    }
+];
---
+++ desktop.bundles/hello/hello.deps.js
@@ -0,0 +1,227 @@
+exports.deps = [
+    {
+        "block": "page"
+    },
+    {
+        "block": "i-bem-dom"
+    },
+    {
+        "block": "inherit"
+    },
+    {
+        "block": "jquery"
+    },
+    {
+        "block": "loader"
+    },
+    {
+        "block": "loader",
+        "mod": "type"
+    },
+    {
+        "block": "jquery",
+        "elem": "config"
+    },
+    {
+        "block": "ua"
+    },
+    {
+        "block": "objects"
+    },
+    {
+        "block": "functions"
+    },
+    {
+        "block": "dom"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "init"
+    },
+    {
+        "block": "i-bem"
+    },
+    {
+        "block": "i-bem",
+        "elem": "internal"
+    },
+    {
+        "block": "identify"
+    },
+    {
+        "block": "next-tick"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "events"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "collection"
+    },
+    {
+        "block": "i-bem",
+        "elem": "collection"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "events",
+        "mod": "type"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "events",
+        "mod": "type",
+        "val": "bem"
+    },
+    {
+        "block": "events"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "init",
+        "mod": "auto"
+    },
+    {
+        "block": "page",
+        "elem": "css"
+    },
+    {
+        "block": "page",
+        "elem": "js"
+    },
+    {
+        "block": "page",
+        "mod": "theme"
+    },
+    {
+        "block": "page",
+        "mod": "theme",
+        "val": "body"
+    },
+    {
+        "block": "body-inner"
+    },
+    {
+        "block": "section"
+    },
+    {
+        "block": "top-header"
+    },
+    {
+        "block": "top-header",
+        "elem": "logo"
+    },
+    {
+        "block": "top-header",
+        "elem": "search-form"
+    },
+    {
+        "block": "top-header",
+        "elem": "phone"
+    },
+    {
+        "block": "menu-section"
+    },
+    {
+        "block": "menu"
+    },
+    {
+        "block": "control"
+    },
+    {
+        "block": "jquery",
+        "elem": "event"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointerclick"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointernative"
+    },
+    {
+        "block": "keyboard",
+        "elem": "codes"
+    },
+    {
+        "block": "menu",
+        "mod": "focused"
+    },
+    {
+        "block": "menu",
+        "elem": "item"
+    },
+    {
+        "block": "menu",
+        "elem": "group"
+    },
+    {
+        "block": "menu",
+        "elem": "group-title"
+    },
+    {
+        "block": "header"
+    },
+    {
+        "block": "content-section"
+    },
+    {
+        "block": "content"
+    },
+    {
+        "block": "content",
+        "elem": "news"
+    },
+    {
+        "block": "content",
+        "elem": "content-info"
+    },
+    {
+        "block": "footer"
+    },
+    {
+        "block": "loader",
+        "mod": "type",
+        "val": "js"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "events",
+        "mod": "type",
+        "val": "dom"
+    },
+    {
+        "block": "i-bem-dom",
+        "elem": "init",
+        "mod": "auto",
+        "val": true
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointer"
+    },
+    {
+        "block": "jquery",
+        "elem": "event",
+        "mod": "type",
+        "val": "pointerpressrelease"
+    },
+    {
+        "block": "menu",
+        "mod": "focused",
+        "val": true
+    }
+];
